9b1154b624dd71adb19ac8b1d54a52bc116c5e7a
GOPIKA-S-S <gopikass1024@gmail.com>
2022-08-27 12:52:05 +0530

    warehouse

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { AdminComponent } from './admin/admin.component';
+import { FrontComponent } from './front/front.component';
+import { ServicesComponent } from './services/services.component';
+import { TableComponent } from './table/table.component';
+import { ChartComponent } from './chart/chart.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+
+
+
+const routes: Routes = [
+  {path:'front',component:FrontComponent},
+  {path:'about-us',component:AboutUsComponent},
+  {path:'services',component:ServicesComponent},
+  {path:'admin',component:AdminComponent},
+  {path:'table',component:TableComponent},
+  {path:'chart',component:ChartComponent},
+  {path:'signup',component:SignupComponent}
+  
+
+  
+  
+  
+
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontComponent } from './front/front.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { AdminComponent } from './admin/admin.component';
+import { TableComponent } from './table/table.component';
+import { ChartComponent } from './chart/chart.component';
+import { SignupComponent } from './signup/signup.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    AdminComponent,
+    TableComponent,
+    ChartComponent,
+    SignupComponent,
+    
+    
   ],
   imports: [
     BrowserModule,
